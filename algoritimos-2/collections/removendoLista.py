2fc6774ce266d2049fbb6449b1197030710bba80
kaykf <kaykfelipe071@gmail.com>
2024-03-27 20:20:12 -0300

    maneiras de remover elementos de uma lista

---
+++ algoritimos-2/collections/removendoLista.py
@@ -0,0 +1,14 @@
+minha_lista = [1,2,3,4,5]
+
+listaCopy = minha_lista.copy()
+
+minha_lista.remove(3)
+minha_lista.pop()
+del minha_lista[0]
+
+print(minha_lista)
+print(listaCopy)
+
+listaCopy.clear()
+print(listaCopy)
+
