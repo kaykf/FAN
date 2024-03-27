762f17447b41a4b108fbca9bf927776ad900a40b
kaykf <kaykfelipe071@gmail.com>
2024-03-27 20:43:03 -0300

    1º questão

---
+++ algoritimos-2/collections/exListas/1questao.py
@@ -0,0 +1,13 @@
+par = []
+impar = []
+num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for i in range(21):
+    if i % 2 == 0:
+        par.append(i)
+    else:
+        impar.append(i)
+
+print(num)
+print(par)
+print(impar)
