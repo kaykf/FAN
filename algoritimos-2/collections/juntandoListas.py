2499efedb5403d76735e63a5430e16c561feaffb
kaykf <kaykfelipe071@gmail.com>
2024-03-27 20:10:20 -0300

    juntando listas

---
+++ algoritimos-2/collections/juntandoListas.py
@@ -0,0 +1,21 @@
+minha_lista = [1,2,3,4,5]
+lista2 = ['Algoritimos']
+
+minha_lista2 = minha_lista + lista2
+
+print(minha_lista2)
+
+lista3 = minha_lista.copy()
+print(lista3)
+
+multLista = minha_lista * 2
+print(multLista)
+
+soma = sum(minha_lista)
+print(soma)
+
+mini = min(minha_lista)
+print(mini)
+
+maxi = max(minha_lista)
+print(maxi)
