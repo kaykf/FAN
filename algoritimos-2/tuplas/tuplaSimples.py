bd75fd2da4e8bff277cc5b78d0ea724fef5832de
kaykf <kaykfelipe071@gmail.com>
2024-04-01 20:48:26 -0300

    tuplas básicas

---
+++ algoritimos-2/tuplas/tuplaSimples.py
@@ -0,0 +1,23 @@
+tupla1 = (1,2,3,4,5)
+print(tupla1)
+print(type(tupla1))
+
+tupla2 = 1,2,3,4
+print(tupla2)
+print(type(tupla2))
+
+tupla3 = (5)
+print(tupla3)
+print(type(tupla3))
+
+tupla4 = (5,)
+print(tupla4)
+print(type(tupla4))
+
+tupla5 = 5,
+print(tupla5)
+print(type(tupla5))
+
+tupla6 = tuple(range(11))
+print(tupla6)
+print(type(tupla6))
