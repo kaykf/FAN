44ca3a8001ac68ee99120cb560af10c388a1987b
kaykf <kaykfelipe071@gmail.com>
2024-04-03 19:36:12 -0300

    localidades

---
+++ algoritimos-2/dixionarios/localidades.py
@@ -0,0 +1,9 @@
+localidades = {
+    (30.654,56.963): "Filial Maceio",
+    (17.658,72.458): "Filial São Paulo",
+    (6.748,42335): "Filial Manaus",
+}
+
+print(localidades)
+
+print(type(localidades))
