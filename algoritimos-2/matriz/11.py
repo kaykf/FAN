77e4a558cb8bdf9fb10d897ff6a72a26269f43fb
kaykf <kaykfelipe071@gmail.com>
2024-04-08 21:07:26 -0300

    questões 11 e 12

---
+++ algoritimos-2/matriz/11.py
@@ -0,0 +1,3 @@
+#faça um programa para uma eleição com 3 candidatos, onde cada voto será salvo na lista do proprio candidato depois conte quantos votos teve cada candidato
+
+#Utilize o programa anterior só que dessa vez teremos apenas uma lista que receberá votos do tipo 1,2 e 3 depois o sistema deverá contar a concorrencia de cada tipo de voto
