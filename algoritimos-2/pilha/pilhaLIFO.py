4a6a482d5606ba99b02fc8426436d9127593b67c
kaykf <kaykfelipe071@gmail.com>
2024-04-10 20:56:08 -0300

    pilha

---
+++ algoritimos-2/pilha/pilhaLIFO.py
@@ -0,0 +1,15 @@
+pilha = []
+
+pilha.append(1)
+pilha.append(2)
+pilha.append(3)
+pilha.append(4)
+
+print(pilha)
+print('----------------')
+
+pilha.pop()
+pilha.pop()
+print(pilha)
+print('----------------')
+print(len(pilha))
