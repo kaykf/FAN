4c888b690f500a05080d4202d3d5e3530084a490
kaykf <kaykfelipe071@gmail.com>
2024-06-20 21:00:41 -0300

    aula 20/06

--- front-end/aula19.06/script.js
+++ front-end/aula19.06/script.js
@@ -1,7 +1,7 @@
 const vlrSecreto = Math.floor(Math.random() * 100 + 1);
 var vlrInicial = 1;
 var vlrFinal = 100;
-var vlrTentativa = 5;
+var vlrTentativas = 5;
 
 function chutar(event){
     event.preventDefault();
@@ -9,5 +9,28 @@ function chutar(event){
     var vlrChute = parseInt(document.getElementById("chute").value);
     if(isNaN(vlrChute) || vlrChute < vlrInicial || vlrChute > vlrFinal){
         document.getElementById("avisos").textContent = "ERRO: Digite um valor válido entre "+ vlrInicial+ " e "+ vlrFinal;
+        return;
+    }
+
+    vlrTentativas = vlrTentativas - 1;
+
+    if(vlrChute === vlrSecreto){
+        document.getElementById("avisos").textContent = "PARABÉNS!! Você acertou! Muito Bom!";
+        return;
+    }
+
+    if(vlrChute < vlrSecreto){
+        vlrInicial = vlrChute + 1;
+        document.getElementById("tentativa").textContent = "Você tem " + vlrTentativas + "tentativas para acertar o número de " + vlrInicial +" a " + vlrFinal + ". ";
+        document.getElementById("avisos").textContent = "O número é maior!";
+    }else{
+        vlrFinal = vlrChute - 1
+        document.getElementById("tentativa").textContent ="Você tem " + vlrTentativas + "tentativas para acertar o número de " + vlrInicial +" a " + vlrFinal + ". ";
+        document.getElementById("avisos").textContent = "O número é menor!";
+    }
+
+    if(vlrTentativas === 0){
+        document.getElementById("aviso").textContent = "Acabou suas Tentivas seu trouxa";
+        return;
     }
 }
